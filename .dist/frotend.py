62369b632db84c38aa7899ce913cbdca5d7e92fc
anishh3 <123816436+anishh3@users.noreply.github.com>
2025-08-31 00:02:15 +0530

    without st.sessionstate

--- .dist/frotend.py
+++ .dist/frotend.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
-with st.chat_message('user'):
-    st.text('Hi')
 
-with st.chat_message('assistant'):
-    st.text('how can i help you?')
+message_history = []
+
+  
+for message in message_history:
+      with st.chat_message(message['role']):
+          st.text(message['content'])
     
 user_input=st.chat_input('Type here')
 if user_input:
+    message_history.append({'role':'user' , 'content': user_input})
     with st.chat_message('user'):
-          st.text(user_input)   
+          st.text(user_input) 
+    message_history.append({'role':'assistant' , 'content': user_input})        
+    with st.chat_message('assistant'):
+        st.text(user_input)      
